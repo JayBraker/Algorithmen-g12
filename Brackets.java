dd1ae49c53e75607f85fb22daadb9939de286162
Laura Mey <laura.mey@rwth-aachen.de>
2022-03-27 17:39:00 +0200

    first commit

--- Brackets.java
+++ Brackets.java
@@ -1,38 +1,76 @@
-import java.util.HashMap;
+package ha1;
+
 import java.util.ArrayDeque;
+import java.util.Arrays;
 
+/* 
+ * class for Hausaufgabenblatt1
+ * 
+ * checks given String for 
+ * valid opening and closing brackets
+ * 
+ * author: Laura Mey
+ * */
 public class Brackets {
-
-	public static boolean isValid(String s) {
-		/*
-		 * Erstelle eine Hashmap die schließende
-		 * und oeffnende Klammern vom gleichen Typ miteinander assoziert.
-		 */
-		HashMap<Character, Character> rbracs = new HashMap<>();
-		rbracs.put(')','(');
-		rbracs.put(']','[');
-		rbracs.put('}','{');
-		ArrayDeque<Character> ad = new ArrayDeque<>();
-
-		for(char c: s.toCharArray()) {
-			if(rbracs.containsValue(c)) { // Handelt es sich um eine oeffende Klammer? Push it!
-				ad.push(c);
-			} else if (rbracs.containsKey(c)) { // Handelt es sich um eine schliessende Klammer?
-				if (ad.peek() != rbracs.get(c)) // Entspricht die letzte offene Klammer auf dem Stack der schließenden?
-					return false; // Wenn nicht, ist der Rhytmus unterbrochen.
-				else
-					ad.pop(); // Ansonsten entferne die letzte offene Klammer, sie wurde geschlossen.
+	
+	/*
+	 * check if an array contains a specific character
+	 */
+	public static boolean charArrContains(char[] arr, char c) {
+		for (char arrc : arr) {
+			if (arrc == c) {
+				return true;
 			}
 		}
-		if (!ad.isEmpty())
-			return false; // Wenn nach durchgehen des String noch Klammern auf dem Stack liegen, wurden nicht alle Klammern geschlossen.
-		return true;
+		return false;
+	}
+
+	/*
+	 * check if the brackets in the given String s are valid
+	 */
+	public static boolean isValid(String s) {
+		
+		char[] opening_brackets = new char[] { '(', '[', '{' };
+		char[] closing_brackets = new char[] { ')', ']', '}' };
+		ArrayDeque<Character> brackets = new ArrayDeque<Character>();
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (charArrContains(opening_brackets, s.charAt(i))) {
+				brackets.add(s.charAt(i));
+			} else if (charArrContains(closing_brackets, s.charAt(i))) {
+				if (brackets.size() > 0) {
+					char brac = brackets.pollLast();
+					//detect matching closing bracket to last opened one
+					int index = Arrays.asList(new Character[] { '(', '[', '{' }).indexOf(brac);
+					if (closing_brackets[index] == s.charAt(i)) {
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+
+			}
+		}
+		
+		// There should be no open brackets left
+		if (brackets.size() == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public static void main(String[] args) {
-		String[] stringArr = {"( ( [ [ ] ] ) )", "( [ ) ]", "( [ ] ] )", "( ( ) ) )", "( ( )", "( { [ ] ) }", "Josha Bartscch"};
-		for(String s: stringArr){
-			System.out.println(String.format("%-15s: %15s", s, String.valueOf(isValid(s))));
-		}
+		System.out.println(isValid("(([[]]))"));
+		System.out.println(isValid("([)]"));
+		System.out.println(isValid("([[])"));
+		System.out.println(isValid("(()))"));
+		System.out.println(isValid("(()"));
+		System.out.println(isValid("( { [ ] ) }"));
+		System.out.println(isValid("adfkjlskdf"));
+		System.out.println(isValid("as[df(asdfsdf)k{s}l]d"));
+		System.out.println(isValid("asdf(asdfsdf)ksl[d"));
 	}
 }
