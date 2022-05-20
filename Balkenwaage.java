2695fc668cdbde2c0243b2e7282f203b6e2395cf
Laura Mey <laura.mey@rwth-aachen.de>
2022-05-20 18:58:32 +0200

    corrected version (only bug left: testcases other than 4 print duplicates of solutions)

--- Balkenwaage.java
+++ Balkenwaage.java
@@ -1,12 +1,14 @@
 package ha9;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * HA 9 
+ * HA 9
  * 
- * Class that provides solutions to a user who wants to measure a target weight using a pan scale and specified measurement weights. * 
+ * Class that provides solutions to a user who wants to measure a target weight
+ * using a pan scale and specified measurement weights. 
  * 
  * @author Josha Bartsch, Christian Thelen, Laura Mey
  *
@@ -14,65 +16,63 @@ import java.util.Scanner;
 public class Balkenwaage {
 
 	/**
-	 * helper function for a recursive call to check if weights can be used to measure the target weight
+	 * helper function for a recursive call to check if weights can be used to
+	 * measure the target weight
 	 * 
-	 * @param weights list of available measurement weights
-	 * @param target weight that should be measured
-	 * @param currentweight weight that will increase during recursion to check if target has been matched
-	 * @param result list that saves used weights to return the result
-	 * @return true iff the target can be measured
+	 * @param weights       list of available measurement weights
+	 * @param target        weight that should be measured
+	 * @param result        array that saves used weights to return the result and check if it totals the target weight
 	 */
-	private static boolean canBeMeasured(ArrayList<Integer> weights, int target, int currentweight, ArrayList<Integer> result) {
-		if (currentweight == target) {
-			return true;
-		}
-		if (weights.isEmpty()) {
-			return false;
-		}
-		else {
-			for (int i = 0; i < weights.size(); i++) {
-				int use_weight = weights.get(i);
-				weights.remove(i);
-				if (canBeMeasured(weights, target, currentweight + use_weight, result)) { //add to left side
-					result.add(use_weight);
-					currentweight += use_weight;
-					return true;
-				}
-				if (canBeMeasured(weights, target, currentweight - use_weight, result)) { //add to right side
-					result.add(use_weight*(-1));
-					currentweight -= use_weight;
-					return true;
-				}
-				if (canBeMeasured(weights, target, currentweight, result)) { //don't add
-					return true;
-				}
-				weights.add(use_weight);
+	private static void canBeMeasured(ArrayList<Integer> weights, int target, int[] result) {
+
+		if (Arrays.stream(result).sum() == target) {
+			// break condition for the recursion - return results
+			System.out.println(Arrays.toString(result));
+			int[] result_symmetric = new int[result.length];
+			for (int i = 0; i < result.length; i++) {
+				result_symmetric[i] = (result[i] * (-1));
 			}
+			System.out.println(Arrays.toString(result_symmetric));
 		}
-		return false;
-	}
-	
-	/**
-	 * prints the result of the call of the recursive function with start weight at 0 and empty start result list
-	 * 
-	 * @param weights list of available measurement weights
-	 * @param target weight that should be measured
-	 */
-	public static void canBeMeasured(ArrayList<Integer> weights, int target) {
-		ArrayList<Integer> result = new ArrayList<>();
-		int currentweight = 0;
-		canBeMeasured(weights, target, currentweight, result);
-		System.out.println(result);
-		ArrayList<Integer> result_symmetric = new ArrayList<>();
-		for (int i : result) {
-			result_symmetric.add(i*(-1));
+
+		if (!weights.isEmpty()) {
+			//as long as there are any left, pick a weight to check
+			int use_weight = weights.get(weights.size() - 1);
+			int index = weights.size() - 1;
+			weights.remove(weights.size() - 1);
+			
+			//put it left
+			result[index] = use_weight;
+			canBeMeasured(weights, target, result);
+
+			//put it right
+			result[index] = -use_weight;
+			canBeMeasured(weights, target, result);
+
+			//decide not to use it
+			result[index] = 0;
+			canBeMeasured(weights, target, result);
+
+			// backtrack
+			weights.add(use_weight);
+
 		}
-		System.out.println(result_symmetric);
 	}
 
 	/**
-	 * Main (test) method that provides measurement weights and asks user for input for the target weight
-	 * and returns solutions
+	 * call the recursive function with an empty result array to check if target can be weighted
+	 * 
+	 * @param weights list of available measurement weights
+	 * @param target  weight that should be measured
+	 */
+	public static void canBeMeasured(ArrayList<Integer> weights, int target) {
+		int[] result = new int[weights.size()];
+		canBeMeasured(weights, target, result);
+	}
+
+	/**
+	 * Main test method that provides measurement weights and asks user for input
+	 * for the target weight and returns solutions
 	 * 
 	 * @param args
 	 */
@@ -82,19 +82,16 @@ public class Balkenwaage {
 		weights.add(8);
 		weights.add(3);
 		weights.add(1);
-		
+
 		System.out.println("Please enter an integer value for the target weight: ");
 		Scanner sc = new Scanner(System.in);
 		int target = sc.nextInt();
 		sc.close();
 		System.out.println("You have the following weights to perform the measurement: " + weights);
-		System.out.println("Here are the solutions (if there are any): ");
-		
-		// int target = 16; //test case
-		
+		System.out.println("The solutions are: ");
+
 		canBeMeasured(weights, target);
-		
-		
+
 	}
 
 }
