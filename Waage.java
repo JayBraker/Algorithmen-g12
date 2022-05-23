1b220b1aa8d4bc4fbcb16b235ee19abb279212b4
JayBraker <>
2022-05-23 16:26:08 +0200

    Changed output format: Always display the sign Do not show equation but all possible combinations

--- Waage.java
+++ Waage.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Waage {
-    private final static int[] weights = {1,3,8,20};
+    private final static int[] weights = {1, 3, 8, 20};
 
     public static void getWeightCombinations(ArrayList<String> basket, int weight) {
         getWeightCombinations(basket, 0, weight, new ArrayList<>(), 0);
     }
 
-    public static void getWeightCombinations(ArrayList<String> basket, int cWeight, int weight, ArrayList<String> comb, int wIndex) {
+    public static void getWeightCombinations(ArrayList<String> basket, int cWeight, int weight,
+            ArrayList<String> comb, int wIndex) {
         if (Math.abs(cWeight) == weight) {
-            basket.add(String.join(", ", comb));
+            basket.add(String.join(",", comb));
             return;
         } else if (wIndex < weights.length) {
             ArrayList<String> tList = new ArrayList<>(comb);
-            tList.add((comb.size() > 0 ? "+": "") +String.valueOf(weights[wIndex]));
-            getWeightCombinations(basket, cWeight+weights[wIndex], weight, tList, wIndex+1);
-            
+            tList.add("+" + String.valueOf(weights[wIndex]));
+            getWeightCombinations(basket, cWeight + weights[wIndex], weight, tList, wIndex + 1);
+
             tList.remove(tList.size() - 1);
-            getWeightCombinations(basket, cWeight, weight, tList, wIndex+1);
-            
-            tList.add(String.valueOf(-1*weights[wIndex]));
-            getWeightCombinations(basket, cWeight-weights[wIndex], weight, tList, wIndex+1);
-        } else 
+            getWeightCombinations(basket, cWeight, weight, tList, wIndex + 1);
+
+            tList.add(String.valueOf(-1 * weights[wIndex]));
+            getWeightCombinations(basket, cWeight - weights[wIndex], weight, tList, wIndex + 1);
+        } else
             return;
     }
 
@@ -35,8 +37,14 @@ public class Waage {
         ArrayList<String> combinations = new ArrayList<>();
         getWeightCombinations(combinations, weight);
 
-        System.out.printf("Es gibt %d mögliche Gewichts-%s:\n", combinations.size(), combinations.size() > 1 ? "Kombinationen" : "Kombination");
-        for (String s : combinations)
-            System.out.println(String.format("%s = %d", s, weight));
+        System.out.printf("Es gibt %d mögliche Loesungen:\n", combinations.size());
+        System.out.println(
+                "(positive Gewichte liegen in der linken, negative in der rechten Schale)");
+        for (Iterator<String> si = combinations.iterator(); si.hasNext();) {
+            System.out.printf("(%s)", si.next());
+            if (si.hasNext())
+                System.out.print(", ");
+        }
+        System.out.println();
     }
 }
