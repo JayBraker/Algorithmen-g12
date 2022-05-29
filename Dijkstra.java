cd67823dd54840b63f235ccf0edac319df883083
Josha <>
2022-05-30 00:52:44 +0200

    Improved table formatting for custom indents/ column size

--- Dijkstra.java
+++ Dijkstra.java
@@ -3,8 +3,20 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
+/**
+ * Implementation of Dijkstra's algorithm.
+ * 
+ * @author Laura Mey, Christian Thelen, Josha Bartsch
+ */
 public class Dijkstra {
 
+	public final static int MAX_DIGITS  = 2;
+	public final static int INDENT  = MAX_DIGITS + 1;
+
+	/**
+	 * Datastructure representing a Node in the Graph
+	 * and its total distance to the initial node ("1").
+	 */
 	private static class Node {
 		int pos;
 		int dist;
@@ -23,6 +35,10 @@ public class Dijkstra {
 		}
 	}
 
+	/**
+	 * Datastructure representing an edge to some vertex
+	 * and the weight assigned.
+	 */
 	private static class Edge {
 		int dest;
 		int dist;
@@ -37,6 +53,13 @@ public class Dijkstra {
 		}
 	}
 
+	/**
+	 * Helper to create a adjacency list, instead of neighbouring vertices
+	 * store edges (neighbour and weight).
+	 * 
+	 * @param vertices Array Following the pattern (# of vertices),(From vertex, To vertex, weight)+
+	 * @return adjacency lists
+	 */
 	@SuppressWarnings({"unchecked"})
 	private static ArrayList<Edge>[] getAdj(int[] vertices) {
 		ArrayList<Edge>[] adj = new ArrayList[vertices[0] + 1];;
@@ -53,13 +76,18 @@ public class Dijkstra {
 		return adj;
 	}
 
+	/**
+	 * Helper to print Array 
+	 * @param arr
+	 * @return
+	 */
 	private static String padArr(int[] arr) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 2; i < arr.length; i++) {
 			if (arr[i] == Integer.MAX_VALUE || arr[i] == -1)
-				sb.append(String.format("%3s", "--"));
+				sb.append(String.format("%"+INDENT+"s", "--"));
 			else
-				sb.append(String.format("%3d", arr[i]));
+				sb.append(String.format("%"+INDENT+"d", arr[i]));
 		}
 		return sb.toString();
 	}
@@ -89,11 +117,13 @@ public class Dijkstra {
 			header[i] = i;
 		}
 
+		// Printing the table head
 		System.out.println(String.format("vi|%s|%s|", padArr(header), padArr(header)));
 		StringBuilder line = new StringBuilder();
-		for (int i = 0; i < (2*(vertexC-1)*3 + 5);i++)
+		for (int i = 0; i < (2*(vertexC-1)*INDENT + 3+ INDENT);i++)
 			line.append("-");
 		System.out.println(line);
+
 		while (!vertices.isEmpty()) {
 			Node n = vertices.remove();
 			if (fixPositions.contains(n.pos))
@@ -116,7 +146,7 @@ public class Dijkstra {
 
 	public static void main(String[] args) {
 		printDijkstra(new int[] {4, 1, 2, 2, 1, 4, 5, 2, 4, 1, 2, 3, 4, 3, 1, 1, 4, 3, 1});
-		
+
 		printDijkstra(new int[] {10,1,2,30,1,3,10,2,5,15,2,8,55,3,4,5,3,9,35,4,2,10,4,5,45,4,6,10,5,3,20,5,7,15,5,9,25,6,7,5,7,10,20,8,10,15,9,8,10,9,10,30});
 	}
 }
